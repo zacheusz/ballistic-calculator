b5c2a55b1c565189e50275c699342b01629105bc
zacheusz <zacheusz.siedlecki@gmail.com>
2025-05-28 14:10:01 +0200

    :rotating_light: Fix linting errors in test files
    - Added ESLint directives to test files to properly recognize Jest globals
    - Updated ESLint configuration with test file overrides
    - Fixed 'no-undef' errors for Jest functions and globals

--- src/components/__tests__/ShotComponent.test.jsx
+++ src/components/__tests__/ShotComponent.test.jsx
@@ -1,3 +1,6 @@
+/* eslint-env jest */
+/* global jest, describe, test, expect, beforeEach */
+
 import React from 'react';
 import { render, screen, fireEvent } from '@testing-library/react';
 import ShotComponent from '../ShotComponent';
